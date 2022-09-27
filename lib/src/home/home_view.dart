6fbcbac533a9a6eda7f352d738ce75727dd5cc54
Matvin <matheusviniciusf@gmail.com>
2022-09-27 18:07:00 -0300

    fix: Added dispose to controllers and autorun

--- lib/src/home/home_view.dart
+++ lib/src/home/home_view.dart
@@ -5,6 +5,7 @@ import 'package:learning_project/src/home/widgets/filmListViewItem.dart';
 import 'package:learning_project/src/home/home_controller.dart';
 import 'package:learning_project/src/models/film.dart';
 import 'package:mobx/mobx.dart';
+import 'package:flutter/src/widgets/basic.dart' as basic;
 
 import '../config/themes/theme_controller.dart';
 import '../services/ghibli_service/ghibli_service.dart';
@@ -21,15 +22,23 @@ class _HomeViewState extends State<HomeView> {
   var scrollController = ScrollController();
   var themeController = GetIt.I.get<ThemeController>();
   late ThemeData currentTheme;
+  late ReactionDisposer autorunDisposable;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     homeController.checkLogin();
     if (!homeController.isLoggedIn) Navigator.of(context).pop();
     homeController.fetchFilms();
     currentTheme = themeController.currentTheme;
+
+    scrollController.addListener(() {
+      if (scrollController.position.pixels >
+              scrollController.position.maxScrollExtent - 50 &&
+          !homeController.isFetchingFilmes) {
+        homeController.fetchFilms();
+      }
+    });
   }
 
   @override
@@ -37,7 +46,7 @@ class _HomeViewState extends State<HomeView> {
     // TODO: implement didChangeDependencies
     super.didChangeDependencies();
 
-    autorun(
+    autorunDisposable = autorun(
       (_) {
         if (!homeController.isLoggedIn) {
           Navigator.of(context).pop();
@@ -48,87 +57,89 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    scrollController
-      ..addListener(() {
-        if (scrollController.position.pixels >
-                scrollController.position.maxScrollExtent - 50 &&
-            !homeController.isFetchingFilmes) {
-          homeController.fetchFilms();
-        }
-      });
-
-    return Scaffold(body: Observer(
-      builder: (context) {
-        return Stack(
-          children: [
-            AppBar(
-                centerTitle: true,
-                title: Text('Ghibli', style: currentTheme.textTheme.titleLarge),
-                automaticallyImplyLeading: false,
-                actions: [
-                  IconButton(
-                      onPressed: () {
-                        homeController.logout();
-                      },
-                      icon: const Icon(
-                        Icons.logout,
-                      ))
-                ]),
-            Padding(
-              padding: const EdgeInsets.only(top: 70),
-              child: Container(
-                decoration: const BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(80),
-                        topRight: Radius.circular(80)),
-                    boxShadow: [
-                      BoxShadow(
-                          offset: Offset(0, -5),
-                          blurRadius: 10,
-                          spreadRadius: 0)
-                    ]),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 40, left: 40, right: 40),
-                  child: Stack(
-                    children: [
-                      ListView.builder(
-                        controller: scrollController,
-                        itemCount: homeController.films.length,
-                        itemBuilder: (context, index) {
-                          var film = homeController.films[index];
-                          return FilmListViewItem(film, index);
+    return Scaffold(
+      body: Observer(
+        builder: (context) {
+          return Stack(
+            children: [
+              AppBar(
+                  centerTitle: true,
+                  title:
+                      Text('Ghibli', style: currentTheme.textTheme.titleLarge),
+                  automaticallyImplyLeading: false,
+                  actions: [
+                    IconButton(
+                        onPressed: () {
+                          homeController.logout();
                         },
-                        padding: EdgeInsets.only(top: 15),
-                      ),
-                      homeController.isFetchingFilmes
-                          ? Align(
-                              alignment: Alignment.bottomCenter,
-                              child: Container(
-                                height: 35,
-                                width: 35,
-                                padding: EdgeInsets.all(10),
-                                margin: EdgeInsets.only(bottom: 10),
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(18)),
+                        icon: const Icon(
+                          Icons.logout,
+                        ))
+                  ]),
+              Padding(
+                padding: const EdgeInsets.only(top: 70),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(80),
+                          topRight: Radius.circular(80)),
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, -5),
+                            blurRadius: 10,
+                            spreadRadius: 0)
+                      ]),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(top: 40, left: 40, right: 40),
+                    child: Stack(
+                      children: [
+                        ListView.builder(
+                          controller: scrollController,
+                          itemCount: homeController.films.length,
+                          itemBuilder: (context, index) {
+                            var film = homeController.films[index];
+                            return FilmListViewItem(film, index);
+                          },
+                          padding: EdgeInsets.only(top: 15),
+                        ),
+                        homeController.isFetchingFilmes
+                            ? Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Container(
+                                  height: 35,
+                                  width: 35,
+                                  padding: EdgeInsets.all(10),
+                                  margin: EdgeInsets.only(bottom: 10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(18)),
+                                  ),
+                                  child: CircularProgressIndicator(
+                                    color: currentTheme.primaryColor,
+                                    backgroundColor: Colors.white,
+                                  ),
                                 ),
-                                child: CircularProgressIndicator(
-                                  color: currentTheme.primaryColor,
-                                  backgroundColor: Colors.white,
-                                ),
-                              ),
-                            )
-                          : Container(),
-                    ],
+                              )
+                            : Container(),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        );
-      },
-    ));
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    autorunDisposable();
+    super.dispose();
   }
 }
